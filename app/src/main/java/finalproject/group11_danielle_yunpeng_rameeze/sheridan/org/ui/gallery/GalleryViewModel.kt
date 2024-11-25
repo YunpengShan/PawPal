356ccba8be27d7b3fd18a839ea129dc78eb81024
Yunpeng Shan <shanyunp@sheridancollege.ca>
2024-11-25 16:54:43 -0500

    Initial Commit

---
+++ app/src/main/java/finalproject/group11_danielle_yunpeng_rameeze/sheridan/org/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package finalproject.group11_danielle_yunpeng_rameeze.sheridan.org.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
